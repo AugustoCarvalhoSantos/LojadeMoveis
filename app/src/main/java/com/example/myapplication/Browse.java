e4a2ee1fc6458ea8b81fe3ca82d26143d555b527
AugustoCarvalho <gutinho102004@gmail.com>
2020-09-27 00:40:25 -0300

    Geolocalização feita

--- app/src/main/java/com/example/myapplication/Browse.java
+++ app/src/main/java/com/example/myapplication/Browse.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
 public class Browse extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class Browse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
     }
+    public void Cadastro(View view) {
+        Intent intent = new Intent(this, Cadastro.class);
+        startActivity(intent);
+    }
+    public void Oferta(View view) {
+        Intent intent = new Intent(this, Oferta.class);
+        startActivity(intent);
+    }
+    public void Mesa(View view) {
+        Intent intent = new Intent(this, Mesa.class);
+        startActivity(intent);
+    }
+
 }
