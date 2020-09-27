fb171fb3af8dedcac2282a60dc87fb0ae66723c9
AugustoCarvalho <gutinho102004@gmail.com>
2020-09-27 04:40:39 -0300

    Foto funcionando

--- app/src/main/java/com/example/myapplication/Cadastro.java
+++ app/src/main/java/com/example/myapplication/Cadastro.java
@@ -2,13 +2,37 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Cadastro extends AppCompatActivity {
+
+    private static final int CAMERA = 0;
+    ImageView imgFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        imgFoto = (ImageView)findViewById(R.id.imgFoto);
+    }
+    public void Foto(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!= null){
+            startActivityForResult(takePictureIntent, CAMERA);
+        }
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgFoto.setImageBitmap(imageBitmap);
+        }
     }
 }
